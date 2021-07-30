54db1b7dc11ce2b85dd97d64fa86993f0e677c43
João Pedro <Joaosilva8882@gmail.com>
2021-07-29 22:46:01 -0300

    modificando todo layout da parte front-end

---
+++ back-end/escolaJavaliCansado/src/test/java/escolaJavaliCansado/org/escolaJavaliCansado/EscolaJavaliCansadoApplicationTests.java
@@ -0,0 +1,13 @@
+package escolaJavaliCansado.org.escolaJavaliCansado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaJavaliCansadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
